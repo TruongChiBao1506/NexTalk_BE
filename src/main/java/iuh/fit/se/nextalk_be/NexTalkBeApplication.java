83a3aa919863ee3f334ca99727541133b2076ffa
TruongChiBao <chibaotruong1506@gmail.com>
2026-06-08 23:15:05 +0700

    feat: initialize NexTalk backend project with core services for authentication, messaging, groups, and presence management

---
+++ src/main/java/iuh/fit/se/nextalk_be/NexTalkBeApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.nextalk_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexTalkBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexTalkBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iuh/fit/se/nextalk_be/NexTalkBeApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.nextalk_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexTalkBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
